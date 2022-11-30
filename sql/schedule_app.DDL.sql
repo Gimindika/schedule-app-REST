c800e6863c306af4b4d535c754643d9b867461d2
Gerrit Indika Mulya <gimindika@gmail.com>
2022-11-30 09:57:35 +0700

    SQL DML DDL Update

--- sql/schedule_app.DDL.sql
+++ sql/schedule_app.DDL.sql
@@ -1,55 +1,65 @@
 -- schedule_app.accesses definition
-
 CREATE TABLE `accesses` (
   `access_id` int NOT NULL AUTO_INCREMENT,
   `access_type` varchar(50) NOT NULL,
   PRIMARY KEY (`access_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 34 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- schedule_app.batches definition
-
 CREATE TABLE `batches` (
   `batch_id` int NOT NULL AUTO_INCREMENT,
   `schedule_year` year NOT NULL,
-  `schedule_month` enum('january','february','march','april','may','june','july','august','september','october','november','december') NOT NULL,
+  `schedule_month` enum(
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december'
+  ) NOT NULL,
   PRIMARY KEY (`batch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 26 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- schedule_app.events definition
-
 CREATE TABLE `events` (
   `event_id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
-  `recurrence` enum('monday','tuesday','wednesday','thursday','friday','saturday','sunday') DEFAULT NULL,
+  `recurrence` enum(
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+  ) DEFAULT NULL,
   PRIMARY KEY (`event_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- schedule_app.members definition
-
 CREATE TABLE `members` (
   `member_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `profile_color` varchar(6) NOT NULL,
   PRIMARY KEY (`member_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 28 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- schedule_app.users definition
-
 CREATE TABLE `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `user_email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- schedule_app.schedules definition
-
 CREATE TABLE `schedules` (
   `schedule_id` int NOT NULL AUTO_INCREMENT,
   `schedule_date` date DEFAULT NULL,
@@ -60,11 +70,9 @@ CREATE TABLE `schedules` (
   KEY `schedules_ibfk_2` (`batch_id`),
   CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`event_id`),
   CONSTRAINT `schedules_ibfk_2` FOREIGN KEY (`batch_id`) REFERENCES `batches` (`batch_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 784 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- schedule_app.user_access definition
-
 CREATE TABLE `user_access` (
   `access_id` int NOT NULL,
   `user_id` int NOT NULL,
@@ -72,11 +80,9 @@ CREATE TABLE `user_access` (
   KEY `access_id` (`access_id`),
   CONSTRAINT `user_access_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
   CONSTRAINT `user_access_ibfk_2` FOREIGN KEY (`access_id`) REFERENCES `accesses` (`access_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- schedule_app.`assignment` definition
-
 CREATE TABLE `assignment` (
   `schedule_id` int NOT NULL,
   `member_id` int NOT NULL,
@@ -84,4 +90,4 @@ CREATE TABLE `assignment` (
   KEY `assignment_ibfk_1` (`schedule_id`),
   CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`schedule_id`) ON DELETE CASCADE,
   CONSTRAINT `assignment_ibfk_2` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
---
+++ sql/user_access_202211300956.sql
@@ -0,0 +1,7 @@
+INSERT INTO schedule_app.user_access (access_id,user_id) VALUES
+	 (10,2),
+	 (14,2),
+	 (18,2),
+	 (19,2),
+	 (1,1),
+	 (1,3);
