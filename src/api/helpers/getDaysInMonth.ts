249fb0cd14942da07dea18c3e46eeb076bddd37d
Gerrit Indika Mulya <gimindika@gmail.com>
2022-11-30 07:12:05 +0700

    Reset Schedules on Batch update

--- src/api/helpers/getDaysInMonth.ts
+++ src/api/helpers/getDaysInMonth.ts
@@ -1,46 +1,45 @@
 export const days = {
-  monday: 1,
-  tuesday: 2,
-  wednesday: 3,
-  thursday: 4,
-  friday: 5,
-  saturday: 6,
-  sunday: 0,
+	monday: 1,
+	tuesday: 2,
+	wednesday: 3,
+	thursday: 4,
+	friday: 5,
+	saturday: 6,
+	sunday: 0,
 };
 
 export const months = {
-  january: 1,
-  february: 2,
-  march: 3,
-  april: 4,
-  may: 5,
-  june: 6,
-  july: 7,
-  august: 8,
-  september: 9,
-  october: 10,
-  november: 11,
-  december: 12,
+	january: 1,
+	february: 2,
+	march: 3,
+	april: 4,
+	may: 5,
+	june: 6,
+	july: 7,
+	august: 8,
+	september: 9,
+	october: 10,
+	november: 11,
+	december: 12,
 };
 
 export const getDaysInMonth = (
-  month: number,
-  year: number,
-  dayToFind: number
+	month: number,
+	year: number,
+	dayToFind: number
 ) => {
-  var d = new Date();
+	var getTot = new Date(year, month, 0).getDate(); //Get total days in a month
 
-  var getTot = new Date(month, year, 0).getDate(); //Get total days in a month
+	var datesOfday = new Array(); //Declaring array for inserting Saturdays
 
-  var datesOfday = new Array(); //Declaring array for inserting Saturdays
+	for (var i = 1; i <= getTot; i++) {
+		//looping through days in month
+		var newDate = new Date(year, month - 1, i);
 
-  for (var i = 1; i <= getTot; i++) {
-    //looping through days in month
-    var newDate = new Date(d.getFullYear(), d.getMonth(), i);
-    if (newDate.getDay() == dayToFind) {
-      datesOfday.push(i);
-    }
-  }
+		if (newDate.getDay() == dayToFind) {
+			datesOfday.push(i);
+		}
+	}
 
-  return datesOfday;
+	return datesOfday;
 };
